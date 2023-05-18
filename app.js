df86d979cfb756a9c4cd5642a63bb484aeb3456e
Lucas Gabriel <105455415+Lucasgabdom@users.noreply.github.com>
2023-05-18 20:33:15 -0300

    volta para o começo do arquivo index

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
-console.log ("rodando o sistema de gerenciamento de jogos")
-console.log ("aplicações está se conectanodo ao banco de dados")
-//mais um console log de teste:
-console.log ("Estou com vontade de programar kkkk")
-console.log ("testando o terminal")
+consele.log ("rodando o sistema de gerenciamento de jogos")
