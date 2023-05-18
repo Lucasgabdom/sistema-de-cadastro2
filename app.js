6dfca53acd63e31d357ae76921327472bc479639
Lucas Gabriel <105455415+Lucasgabdom@users.noreply.github.com>
2023-05-18 20:01:06 -0300

    fazendo algumas alterações no js e no html

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-consele.log ("rodando o sistema de gerenciamento de jogos")
+console.log ("rodando o sistema de gerenciamento de jogos")
+console.log ("aplicações está se conectanodo ao banco de dados")
+//mais um console log de teste:
+console.log ("Estou com vontade de programar kkkk")
