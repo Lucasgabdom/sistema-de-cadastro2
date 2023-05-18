59c2352167108206fa0d453bcdd66b924fa2011a
Lucas Gabriel <105455415+Lucasgabdom@users.noreply.github.com>
2023-05-18 20:23:58 -0300

    adicionando mais um console log

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log ("rodando o sistema de gerenciamento de jogos")
 console.log ("aplicações está se conectanodo ao banco de dados")
 //mais um console log de teste:
 console.log ("Estou com vontade de programar kkkk")
+console.log ("testando o terminal")
